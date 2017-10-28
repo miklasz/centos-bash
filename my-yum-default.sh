b7eaed17d2d84d3af4ec663d94684ccd10123823
Bartosz Miklaszewski <bartosz@miklaszewski.com>
2017-10-28 02:21:24 +0100

    Update my-yum-default.sh

--- my-yum-default.sh
+++ my-yum-default.sh
@@ -3,6 +3,11 @@ function log {
   echo "=> $1"  >&2
 }
 log ""
+log "ADD GIT"
+sudo yum -y install git
+log ""
+log "ADD WGET"
+sudo yum -y install wget
 log ""
 log "DOWNLOAD AND ADD HTOP REPO"
 wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-10.noarch.rpm
